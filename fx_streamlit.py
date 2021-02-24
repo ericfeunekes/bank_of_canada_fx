944f93c3ddedcb29fd02a3dc127deb43d06e53ca
Eric Feunekes <eric.feunekes@pwc.com>
2021-02-24 09:12:03 -0400

    docker

--- fx_streamlit.py
+++ fx_streamlit.py
@@ -12,39 +12,36 @@ def get(url):
     return requests.get(url)
 
 
-r = get('https://www.bankofcanada.ca/valet/observations/group/FX_RATES_DAILY/json?start_date=2015-01-03')
+r = get(
+    "https://www.bankofcanada.ca/valet/observations/group/FX_RATES_DAILY/json?start_date=2015-01-03"
+)
 data = json.loads(r.content)
-df = pd.DataFrame.from_dict(data['observations'])
+df = pd.DataFrame.from_dict(data["observations"])
 for i, row in df.iterrows():
     for j, cell in enumerate(row):
         if type(cell) is dict:
-            df.iloc[i, j] = cell['v']
+            df.iloc[i, j] = cell["v"]
 
-df['d'] = pd.to_datetime(df['d'])
+df["d"] = pd.to_datetime(df["d"])
 min_date = df.d.min()
 max_date = df.d.max()
-resampling = {
-    'Year': 'Y',
-    'Month': 'M',
-    'Quarter': 'Q',
-    'Week': 'W',
-    'Day': 'D'
-}
+resampling = {"Year": "Y", "Month": "M", "Quarter": "Q", "Week": "W", "Day": "D"}
 # %%
-st.write('Bank of Canada data')
-date_df = df.set_index('d').astype(float)
+st.title("Bank of Canada data")
+date_df = df.set_index("d").astype(float)
 
-st.sidebar.title('Filter data')
+st.sidebar.title("Filter data")
 fx_rate = st.sidebar.multiselect(
-    'Pick one or more rates', list(date_df.columns), default=['FXUSDCAD', 'FXEURCAD'])
-sample = st.sidebar.selectbox(
-    'Group by', options=list(resampling.keys()), index=2)
-start, end = st.sidebar.date_input('Date Range', value=(min_date, max_date))
+    "Pick one or more rates", list(date_df.columns), default=["FXUSDCAD", "FXEURCAD"]
+)
+sample = st.sidebar.selectbox("Group by", options=list(resampling.keys()), index=2)
+start, end = st.sidebar.date_input("Date Range", value=(min_date, max_date))
 
-option = fx_rate or 'FXUSDCAD'
-sample_rate = resampling[sample] if sample else 'M'
-st.write(date_df.loc[start:end, option].resample(sample_rate).mean())
+option = fx_rate or "FXUSDCAD"
+sample_rate = resampling[sample] if sample else "M"
+
 st.line_chart(data=date_df.loc[start:end, option])
+st.dataframe(date_df.loc[start:end, option].resample(sample_rate).mean(), height=2000)
 
 # Download file
 
@@ -64,8 +61,7 @@ def download_link(object_to_download, download_filename, download_link_text):
 
     """
     if isinstance(object_to_download, pd.DataFrame):
-        object_to_download = object_to_download.to_csv(
-            index=True, index_label='Date')
+        object_to_download = object_to_download.to_csv(index=True, index_label="Date")
 
     # some strings <-> bytes conversions necessary here
     b64 = base64.b64encode(object_to_download.encode()).decode()
@@ -73,7 +69,10 @@ def download_link(object_to_download, download_filename, download_link_text):
     return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
 
-if st.sidebar.button('Download data as CSV'):
+if st.sidebar.button("Download data as CSV"):
     tmp_download_link = download_link(
-        date_df.loc[start:end, option].resample(sample_rate).mean(), 'bocRate.csv', 'Click here to download your data!')
+        date_df.loc[start:end, option].resample(sample_rate).mean(),
+        "bocRate.csv",
+        "Click here to download your data!",
+    )
     st.sidebar.markdown(f"**{tmp_download_link}**", unsafe_allow_html=True)
